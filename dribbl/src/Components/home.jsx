6bfbaf5cd12ff776ab05148e19c7072d26eb2c12
OnShoreApple <shashwatsolanki786@gmail.com>
2025-03-31 23:21:47 +0530

    User Details ka page

--- dribbl/src/Components/home.jsx
+++ dribbl/src/Components/home.jsx
@@ -5,7 +5,52 @@ function Home() {
     <>
       <div className="display">
         <div className="contacts">
-          <div></div>
+          <div className="header">
+            <img
+              src="https://www.freeiconspng.com/thumbs/search-icon-png/search-icon-png-5.png"
+              alt="search"
+              className="search"
+            />
+            <input type="text" className="search-box" id="sbox"></input>
+          </div>
+          <div className="users">
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+            <div className="user">
+              <div className="user-img-num"></div>
+              <div className="user-name">user name</div>
+            </div>
+          </div>
         </div>
         <div className="chats">
           <div className="user-detail">
