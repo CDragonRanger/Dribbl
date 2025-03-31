aae0538fc1e099ba4c212c1f6957e56088a5bafd
OnShoreApple <shashwatsolanki786@gmail.com>
2025-03-31 22:28:20 +0530

    Basic React Structure

---
+++ dribbl/src/Components/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="display">
+        <div className="contacts">
+          <div></div>
+        </div>
+        <div className="chats">
+          <div className="user-detail">
+            <div className="user-img"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Home;
